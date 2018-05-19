2654a84a4ddb8173006f055b685994cf79575af6
SeriousJack666 <robert.damien999@gmail.com>
2018-05-19 16:16:10 +0200

    création Répertoire de contacts

---
+++ version php/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Gestionnaire de contacts</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/script.js"></script>
+  </head>
+  <div class="conteneur">
+
+    <header>
+      <h1>Gestionnaire de contacts</h1>
+      <img src="assets/img/avatar.png" alt="avatar" />
+    </header>
+
+    <body>
+
+      <div class="menuDeroulant">
+
+        <div id="choix">
+
+          <p>
+
+            <select name="destination">
+
+            <option value="">Que voulez vous faire ?</option>
+
+            <option value="">Voir nombre contacts</option>
+
+            <option value="">Listez contacts</option>
+
+            <option value="">Ajoutez contacts</option>
+
+            </select>
+
+
+       <!-- <input type="submit" value="Go" title="valider pour aller à la page sélectionnée" /> -->
+          </p>
+        </div>
+
+        <div id="contenu">
+
+        </div>
+
+
+      </div>
+    </body>
+  </div>
+</html>
