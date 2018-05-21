b3918315d42a93c91e13ac94f09ac759335141d9
SeriousJack666 <robert.damien999@gmail.com>
2018-05-21 21:45:06 +0200

    base solide

---
+++ version Javascript/assets/js/script.js
@@ -0,0 +1,38 @@
+
+
+//=========  Sélection de la div à afficher ======//
+
+var select = document.getElementById("choix_options");
+
+select.addEventListener("change", event => {
+	for ( var item of select){	
+		if (select.selectedIndex == "1"){ 
+			option0.style.display = 'none';
+			option1.style.display = 'block';
+			option2.style.display = 'none';
+			option3.style.display = 'none';
+		}
+		else if (select.selectedIndex == "2"){
+			option0.style.display = 'none';
+			option1.style.display = 'none';
+			option2.style.display = 'block';
+			option3.style.display = 'none'; 			
+		}
+		else if (select.selectedIndex == "3"){
+			option0.style.display = 'none';
+			option1.style.display = 'none';
+			option2.style.display = 'none';
+			option3.style.display = 'block';
+		}
+		else {
+			option0.style.display = 'block';
+			option1.style.display = 'none';
+			option2.style.display = 'none';
+			option3.style.display = 'none';
+		}
+	}
+});	
+
+
+
+
